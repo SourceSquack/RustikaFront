e33ea7cab86aef69a070369fa0f0f03570597094
akanashimi <andresvargas094@gmail.com>
2023-03-28 16:49:18 -0600

    menu cards

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: [
+      "www.eleconomista.com.mx",
+      "editorialtelevisa.brightspotcdn.com",
+      "1.bp.blogspot.com",
+      "www.mexicoenmicocina.com",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
