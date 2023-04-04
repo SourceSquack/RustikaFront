bb86b4d3606c8d69dfc626f19f81ea35f17cfbde
akanashimi <andresvargas094@gmail.com>
2023-04-03 22:25:10 -0600

    carousel in menu

--- client/components/Card.jsx
+++ client/components/Card.jsx
@@ -4,13 +4,15 @@ import React from 'react'
 
 export default function Card({ result }) {
     return (
-        <div className='cursor-pointer sm:p-3 sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200 group'>
+        <div className='card cursor-pointer bg-white w-[200px] h-[400px] m-2 rounded-lg shadow-lg'>
             <Link href={`/menu/${result.id}`}>
-                <Image src={result.imageUrl} width={500} height={300} className='sm:rounded-t-lg group-hover:opacity-80 transition-opacity duration-200' style={{ maxWidth: "100%", height: "auto", }} alt='image is not available'></Image>
-                <div className='p-2'>
-                    <h2 className='truncate text-lg font-bold'>{result.name}</h2>
-                    {result.description ? <p className='line-clamp-2 text-md'>{result.description}</p> : null}
-                    <p className='flex items-center'>$ {result.value}</p>
+                <div className='top'>
+                    <Image src={result.imageUrl} alt={"Imagen menú"} width={200} height={200} className='object-cover p-2 w-[200px] h-[200px]'></Image>
+                </div>
+                <div className='bottom flex flex-col justify-center items-start p-5'>
+                    <h2 className='name text-sm font-bold'>{result.name}</h2>
+                    {result.description ? <p className='description text-xs font-light'>{result.description}</p> : null}
+                    <p className='price'>$ {result.value}</p>
                 </div>
             </Link>
         </div>
--- client/components/Carousel.jsx
+++ client/components/Carousel.jsx
@@ -1,16 +1,29 @@
 import React from 'react'
 import Card from './Card'
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi'
 
 export default function Carousel({ results, title }) {
-    return (<>
-        <div className='text-center py-4 text-xl font-bold'>{title}</div>
-        <div className='carousel p-4 flex items-center justify-start overflow-x-auto'>
-            <div className='sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-6xl mx-auto py-4'>
+    const scrollLeft = () => {
+        document.getElementById('content').scrollLeft -= 400;
+    }
+
+    const scrollRight = () => {
+        document.getElementById('content').scrollLeft += 400;
+    }
+    return (
+        <div className='relative'>
+            <div className='text-center py-4 text-2xl font-bold'>{title}</div>
+            <div className='absolute right-0 top-5'>
+                <button className='p-2 m-2 rounded-full bg-white' onClick={scrollLeft}><FiChevronLeft /></button>
+                <button className='p-2 m-2 rounded-full bg-white' onClick={scrollRight}><FiChevronRight /></button>
+            </div>
+            <div id='content' className='carpusel p-4 flex items-center justify-start overflow-x-auto scroll-smooth'>
                 {results.map((result) => (
-                    <Card key={result.id} result={result} />
+                    <div>
+                        <Card key={result.id} result={result} />
+                    </div>
                 ))}
             </div>
         </div>
-    </>
     )
 }
