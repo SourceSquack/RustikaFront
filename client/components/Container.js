d5abee986237aba2b6f5a6b1cb928027d7f7a1d6
Zetah07 <sebastiancastro0792@gmail.com>
2023-04-13 11:39:41 -0500

    merge change

--- client/components/Container.js
+++ client/components/Container.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 export default function Container({ children }) {
   return (
     <div className="w-screen flex justify-center">
-      <div className="flex px-4 w-full xl:w-xl">
+      <div className="flex px-4 w-full">
         {children}
       </div>
     </div>
