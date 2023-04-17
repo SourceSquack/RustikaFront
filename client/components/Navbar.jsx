9c0d059927cb844dd0cb2f957e1ef6392c8c23a6
JuanRomeroSerna <juanromeroserna@gmail.com>
2023-04-17 16:23:16 -0500

    Feat(reservations with dates and hours locks.
    NavBar correction on to be able to re utilize it in other componets

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,14 +1,12 @@
-import React, { useCallback, useEffect, useState } from "react";
-import NavbarItem from "./NavbarItem";
-import MobileMenu from "./MobileMenu";
-import {
-  ChevronDownIcon,
-} from "@heroicons/react/24/outline";
-import Image from "next/image";
+import React, { useCallback, useEffect, useState } from 'react';
+import NavbarItem from './NavbarItem';
+import MobileMenu from './MobileMenu';
+import { ChevronDownIcon } from '@heroicons/react/24/outline';
+import Image from 'next/image';
 
 const TOP_OFFSET = 66;
 
-const Navbar = () => {
+const Navbar = ({ style }) => {
   const [showMobileMenu, setShowMobileMenu] = useState(false);
   const [showBackground, setShowBackground] = useState(false);
 
@@ -20,13 +18,12 @@ const Navbar = () => {
       } else {
         setShowBackground(false);
       }
-    }
+    };
 
-
-    window.addEventListener("scroll", handleScroll);
+    window.addEventListener('scroll', handleScroll);
 
     return () => {
-      window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener('scroll', handleScroll);
     };
   }, []);
 
@@ -34,37 +31,37 @@ const Navbar = () => {
     setShowMobileMenu((current) => !current);
   }, []);
 
-
-
   return (
-    <nav className="w-full fixed z-40">
+    <nav className='w-full fixed z-40' style={style}>
       <div
-        className={`px-1 md:px-4 py-1 flex md:justify-between items-center transition duration-500 ${showBackground ? "bg-zinc-900 bg-opacity-90" : ""
-          }`}
+        className={`px-1 md:px-4 py-1 flex md:justify-between items-center transition duration-500 ${
+          showBackground ? 'bg-zinc-900 bg-opacity-90' : ''
+        }`}
       >
         <div>
           <Image
-            src="/images/logorustika.png"
+            src='/images/logorustika.png'
             width={110}
             height={110}
-            alt="Logo"
+            alt='Logo'
           />
         </div>
-        <div className="ml-19 mx-4 gap-6 hidden md:flex  text-lg">
-          <NavbarItem label="Home" address="/" />
-          <NavbarItem label="Menú" address="/menu" />
-          <NavbarItem label="Bar" address="/bar" />
-          <NavbarItem label="Reservar" address="/reservar" />
-          <NavbarItem label="Nosotros" address="/about" />
+        <div className='ml-19 mx-4 gap-6 hidden md:flex  text-lg'>
+          <NavbarItem label='Home' address='/' />
+          <NavbarItem label='Menú' address='/menu' />
+          <NavbarItem label='Bar' address='/bar' />
+          <NavbarItem label='Reservar' address='/reservar' />
+          <NavbarItem label='Nosotros' address='/about' />
         </div>
         <div
           onClick={toggleMobileMenu}
-          className="md:hidden flex flex-row-reverse items-center gap-2 ml-8 cursor-pointer relative"
+          className='md:hidden flex flex-row-reverse items-center gap-2 ml-8 cursor-pointer relative'
         >
-          <p className="text-white text-base">Buscar</p>
+          <p className='text-white text-base'>Buscar</p>
           <ChevronDownIcon
-            className={`w-4 text-white fill-white transition ${showMobileMenu ? "rotate-180" : "rotate-0"
-              }`}
+            className={`w-4 text-white fill-white transition ${
+              showMobileMenu ? 'rotate-180' : 'rotate-0'
+            }`}
           />
           <MobileMenu visible={showMobileMenu} />
         </div>
